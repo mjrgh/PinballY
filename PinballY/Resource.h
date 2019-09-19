861d89d6287fbb24d59294811a7f4ad774bb227e
Bryce Yancey <bryce@yanceyware.com>
2019-09-19 16:04:13 -0600

    Add option to mute auto-repeat sounds for prev/next button presses
    Correcting a couple of issues missed on the initial commit.

--- PinballY/Resource.h
+++ PinballY/Resource.h
@@ -595,7 +595,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        2000
-#define _APS_NEXT_COMMAND_VALUE         32876
+#define _APS_NEXT_COMMAND_VALUE         32877
 #define _APS_NEXT_CONTROL_VALUE         1200
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
