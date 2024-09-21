9962bc6465ea8a65835fcc8c7e4d00e7cc300c65
mjrgh <mjr_@hotmail.com>
2024-09-21 12:33:54 -0700

    1.1.0 Beta 8 release

---
+++ BuildAndRelease.bat
@@ -0,0 +1,38 @@
+@echo off
+rem  PinballY Release Builder
+rem
+rem  Run from a Visual Studio CMD prompt window
+rem
+
+rem Clean old builds
+echo ^>^>^> Removing old builds
+msbuild PinballY.sln -t:Clean -p:Configuration=Release;Platform=x86 -v:q -nologo
+if errorlevel 1 goto abort
+msbuild PinballY.sln -t:Clean -p:Configuration=Release;Platform=x64 -v:q -nologo
+if errorlevel 1 goto abort
+
+rem Build the release configurations
+echo.
+echo ^>^>^> Building Release^|x86
+msbuild PinballY.sln -t:Build -p:Configuration=Release;Platform=x86 -v:q -nologo
+if errorlevel 1 goto abort
+
+echo.
+echo ^>^>^> Building Release^|x64
+msbuild PinballY.sln -t:Build -p:Configuration=Release;Platform=x64 -v:q -nologo
+if errorlevel 1 goto abort
+
+
+rem Run the release ZIP builder
+echo.
+echo ^>^>^> Building release packages
+call .\release.bat
+
+echo.
+echo ^>^>^> Release completed
+goto EOF
+
+:abort
+echo MSBUILD exited with error - aborted
+
+:EOF
